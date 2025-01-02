3bb23fd5da43fbb3bf07ae060e073c2a0114fd58
Muditha Madusith <108977790+muditha-madusith@users.noreply.github.com>
2025-01-02 18:21:08 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const fetchBooks = async (page: number): Promise<Book[]> => {
   const startIndex = (page - 1) * 10;
   try {
     const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=react=free-ebooks&startIndex=${startIndex}&maxResults=12&key=YourApiKey`
+      `https://www.googleapis.com/books/v1/volumes?q=devops=free-ebooks&startIndex=${startIndex}&maxResults=12&key=AIzaSyD1rS30pQ3_2H7TOOiIYGgq_cuhh7OPUFc`
     );
     if (!response.ok) {
       throw new Error("Failed to fetch books");
@@ -50,7 +50,7 @@ const Home: React.FC = () => {
 
   return (
     <div>
-      <h1 className="page-title">Books App</h1>
+      <h1 className="page-title">DevOps Books Collection</h1>
       {loading ? (
         <div className="main">
           <div className="loader"></div>
